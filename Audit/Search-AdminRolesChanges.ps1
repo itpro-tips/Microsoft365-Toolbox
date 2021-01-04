38eb5974b8b29a0b5b24ece933d109de963ec829
itpro-tips <44789311+itpro-tips@users.noreply.github.com>
2021-01-04 12:32:21 +0100

    Update Search-AdminRoleChanges

--- Audit/Search-AdminRolesChanges.ps1
+++ Audit/Search-AdminRolesChanges.ps1
@@ -25,6 +25,7 @@ For more Office 365/Microsoft 365 tips and news, check out ITPro-Tips.com.
 
 Version history:
 V1.0, 17 august 2020 - Initial version
+V.1.1, 4 january 2020 - Add ObjectUserPrincipalName
 
 Copyright (c) 2020 Bastien Perez (ITPro-Tips.com)
 
@@ -42,46 +43,87 @@ DEALINGS IN THE SOFTWARE.
 function Search-AdminRoleChanges {
 	[CmdletBinding()]
 	param (
+		[string[]]$ObjectIDs
 	)
     
-    try {
-        Import-Module exchangeonlinemanagement -ErrorAction stop
-    }
-    catch {
-        Write-Warning 'First, install the official Microsoft Exchange Online Management module : Install-Module exchangeonlinemanagement'
-        return
-    }
+	try {
+		Import-Module exchangeonlinemanagement -ErrorAction stop
+	}
+	catch {
+		Write-Warning 'First, install the official Microsoft Exchange Online Management module : Install-Module ExchangeOnlineManagement'
+		exit
+	}
 
-    try {
-        $records = Search-UnifiedAuditLog -StartDate (Get-Date).AddDays(-90) -EndDate (Get-Date).AddDays(1) -Operations ('Add Member to Role', 'Remove Member From Role') -ResultSize 2000 -Formatted-ErrorAction Stop
-    }
-    catch {
-        Connect-ExchangeOnline
-        $records = Search-UnifiedAuditLog -StartDate (Get-Date).AddDays(-90) -EndDate (Get-Date).AddDays(1) -Operations ('Add Member to Role', 'Remove Member From Role') -ResultSize 2000 -Formatted
-    }
+	try {
+		$null = Get-Command Search-UnifiedAuditLog -ErrorAction Stop
+	}
+	catch {
+		Write-Host 'Connect to Exchange Online'-ForegroundColor Cyan
+		
+		try {
+			Connect-ExchangeOnline -ShowBanner:$false -ErrorAction Stop
+		}
+		catch {
+			Write-Warning 'Unable to connect to Exchange Online'
+			exit
+		}
+	}
+	
+	try {
+		$maxAdminLogAge = [System.TimeSpan]::Parse((Get-AdminAuditLogConfig).AdminAuditLogAgeLimit).Days
+
+		Write-Host 'Search Add/Remove Member to Role actions logs'-ForegroundColor Cyan
+
+		if($ObjectIDs) {
+			$objects = New-Object System.Collections.Generic.List[String]
+			foreach($obj in $ObjectIDs) {
+				$user = Get-User $obj
+				$null = $objects.Add($user.UserPrincipalName)
+				$null = $objects.Add($user.ExternalDirectoryObjectId)
+		}
+				
+		}
+		else{
+			# Set tp $null to search All
+			$objects = $null 
+		}
+
+		$records = Search-UnifiedAuditLog -StartDate (Get-Date).AddDays(-$maxAdminLogAge) -EndDate (Get-Date).AddDays(1) -Operations ('Add Member to Role', 'Remove Member From Role') -ResultSize 2000 -Formatted -ObjectIds $objects
+	}
+	catch {
+		Write-Warning "Unable to gather information $($_.Exception.Message)"
+		exit
+	}
 
 	if ($records.Count -eq 0) {
-		Write-Host 'No audit logs found'
+		Write-Host 'No audit logs found' -ForegroundColor Green
 	}
 	else {
-		Write-Host "Processing $($Records.Count) audit records..."
+		
+		Write-Host "Processing $($Records.Count) audit records..." -ForegroundColor Cyan
    
 		$report = New-Object System.Collections.Generic.List[Object]
 		ForEach ($record in $records) {
 			$auditData = ConvertFrom-Json $record.Auditdata
-			# Only process the additions of guest users to groups
 	
 			$timeStamp = Get-Date $record.CreationDate -format g
-			# Try and find the timestamp when the Guest account was created in AAD
-	
-			$object = [PSCustomObject]@{
-				TimeStamp      = $timeStamp
+
+			# Test if ObjectID is GUID or the UserPrincipalName
+			if($auditData.ObjectID -match '(?im)^[{(]?[0-9A-F]{8}[-]?(?:[0-9A-F]{4}[-]?){3}[0-9A-F]{12}[)}]?$'){
+				$objectID = $auditData.ObjectID
+			}
+			else{
+				$objectID = ($auditData.Target | Where-Object {$_.Type -eq 2 -and $_.ID -match '(?im)^[{(]?[0-9A-F]{8}[-]?(?:[0-9A-F]{4}[-]?){3}[0-9A-F]{12}[)}]?$'}).ID # several values are with Type=2 ('User_GUID', 'GUID', 'User', 'NetID') The good value is a GUID so we filter one the value which matches GUID
+			}
 			
-				ObjectId       = $auditData.ObjectId
-				Action         = $auditData.Operation
-				Actor          = $auditData.UserId
+			$object = [PSCustomObject]@{
+				TimeStamp      	= $timeStamp
+				ObjectId       	= $objectID
+				ObjectUserPrincipalName	= ($auditData.Target | Where-Object {$_.Type -eq 5}).Id # value is @{ID=xxx@domain; Type=5
+				RoleName       	= $auditData.modifiedproperties.newvalue[1]
+				Action         	= $auditData.Operation
+				Actor          	= $auditData.UserId
 				ActorIpAddress	= $auditData.ActorIpAddress
-				RoleName       = $auditData.modifiedproperties.newvalue[1]
 			}      
 	   
 			$report.Add($object)
